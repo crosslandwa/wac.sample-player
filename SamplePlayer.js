7b8db70dc9704a657c444be42f2523aca735b0da
Will <crossland.w.a@gmail.com>
2016-08-05 23:36:03 +0100

    Use camelCase throughout

--- SamplePlayer.js
+++ SamplePlayer.js
@@ -3,24 +3,24 @@
 const EventEmitter = require('events'),
     util = require('util');
 
-function SamplePlayer(asset_url, audio_context) {
+function SamplePlayer(assetUrl, audioContext) {
     EventEmitter.call(this);
     let player = this,
         _loaded = false,
         _buffer,
         _voices = [],
-        _playback_rate = 1,
-        _gain_node = audio_context.createGain();
+        _playbackRate = 1,
+        _gainNode = audioContext.createGain();
 
-    this._assetUrl = asset_url;
+    this._assetUrl = assetUrl;
 
-    this.connect = _gain_node.connect.bind(_gain_node);
+    this.connect = _gainNode.connect.bind(_gainNode);
 
-    this.disconnect = _gain_node.disconnect.bind(_gain_node);
+    this.disconnect = _gainNode.disconnect.bind(_gainNode);
 
     this.toMaster = function() {
         player.disconnect();
-        player.connect(audio_context.destination);
+        player.connect(audioContext.destination);
         return player;
     }
 
@@ -31,25 +31,25 @@ function SamplePlayer(asset_url, audio_context) {
     this.play = function(gain) {
         if (!_loaded) return;
 
-        var now = time_now(audio_context),
-            start_time = now,
+        var now = timeNow(audioContext),
+            startTime = now,
             _gain = (gain && (typeof gain.toAbsolute === 'function')) ? gain : { toAbsolute: function() { return 1 } };
 
         if (player.isPlaying()) {
-            _gain_node.gain.cancelScheduledValues(now);
-            anchor(_gain_node.gain, now);
-            _gain_node.gain.linearRampToValueAtTime(0, now + 0.01);
-            start_time = now + 0.01;
+            _gainNode.gain.cancelScheduledValues(now);
+            anchor(_gainNode.gain, now);
+            _gainNode.gain.linearRampToValueAtTime(0, now + 0.01);
+            startTime = now + 0.01;
             player.emit('stopped');
         }
 
-        var source = audio_context.createBufferSource();
-        source.connect(_gain_node);
+        var source = audioContext.createBufferSource();
+        source.connect(_gainNode);
 
-        _gain_node.gain.setValueAtTime(0, start_time);
-        _gain_node.gain.linearRampToValueAtTime(_gain.toAbsolute(), start_time + 0.01);
+        _gainNode.gain.setValueAtTime(0, startTime);
+        _gainNode.gain.linearRampToValueAtTime(_gain.toAbsolute(), startTime + 0.01);
 
-        source.playbackRate.setValueAtTime(_playback_rate, start_time);
+        source.playbackRate.setValueAtTime(_playbackRate, startTime);
         source.buffer = _buffer;
 
         source.addEventListener('ended', () => {
@@ -58,41 +58,41 @@ function SamplePlayer(asset_url, audio_context) {
         });
 
         _voices.push(source);
-        source.start(start_time);
+        source.start(startTime);
         player.emit('started', _gain);
     }
 
     this.updatePlaybackRate = function(rate) {
-        _playback_rate = rate;
-        var now = time_now(audio_context);
+        _playbackRate = rate;
+        var now = timeNow(audioContext);
         _voices.forEach((source) => {
-            source.playbackRate.setValueAtTime(_playback_rate, now);
+            source.playbackRate.setValueAtTime(_playbackRate, now);
         });
     }
 
-    loadSample(asset_url, audio_context, (buffer) => {
+    loadSample(assetUrl, audioContext, (buffer) => {
         _buffer = buffer;
         _loaded = true;
     });
 }
 util.inherits(SamplePlayer, EventEmitter);
 
-function loadSample(asset_url, audio_context, done) {
+function loadSample(assetUrl, audioContext, done) {
     var request = new XMLHttpRequest();
-    request.open('GET', asset_url, true);
+    request.open('GET', assetUrl, true);
     request.responseType = 'arraybuffer';
     request.onload = function () {
-        audio_context.decodeAudioData(request.response, done);
+        audioContext.decodeAudioData(request.response, done);
     }
     request.send();
 }
 
-function anchor(audio_param, now) {
-    audio_param.setValueAtTime(audio_param.value, now);
+function anchor(audioParam, now) {
+    audioParam.setValueAtTime(audioParam.value, now);
 }
 
-function time_now(audio_context) {
-    return audio_context.currentTime;
+function timeNow(audioContext) {
+    return audioContext.currentTime;
 }
 
 module.exports = SamplePlayer;
